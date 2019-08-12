501656a8ba8cdd4b843c86854f8c1b15c934f68f
Kirill Bugaev <kirill.bugaev87@gmail.com>
2019-08-13 04:47:12 +0800

    gui improved

--- build/js/gui.js
+++ build/js/gui.js
@@ -30,223 +30,297 @@ function getStyle(el, styleProp) {
   }
 }
 // ============
-function loadStyle() {
-	let sel = document.getElementById("theme-sel");
-	sel.selectedIndex = document.cookie;
+
+const taskColors = ["default", "red", "green", "yellow", "blue", "purple", "aqua"];
+const btnHeightAdd = 4;
+const btnWidthAdd = 0;
+var _taskIndex = 0;
+
+function equalizeBtnSize() {
+	let btnEqualizer = document.getElementById("btn-equalizer");
+	let eqBtnHeight = getStyle(btnEqualizer, "height");
+	let eqBtnWidth = getStyle(btnEqualizer, "width");
+	let btnHeight = (Math.round(parseFloat(eqBtnHeight, 10)) + btnHeightAdd) + "px";
+	let btnWidth = (Math.round(parseFloat(eqBtnWidth, 10)) + btnWidthAdd) + "px";
+	let btnVarsCssDeclar = "--btn-height:" + btnHeight + ";--btn-width:" + btnWidth + ";";
+	let cssDeclarBlock = ":root{" + btnVarsCssDeclar + "}";
+	let style = document.createElement("style");
+	style.type = "text/css";
+	style.innerHTML = cssDeclarBlock;
+	document.getElementsByTagName("head")[0].appendChild(style);
+	btnEqualizer.parentNode.removeChild(btnEqualizer);
+}
+
+function loadStyles() {
+	let select = document.getElementById("theme-select");
+	if (document.cookie == "")
+		select.selectedIndex = 0;
+	else
+		select.selectedIndex = document.cookie;
 	let link = document.createElement("link");
 	link.id = "theme-style";
-	link.href = "./css/" + sel.value + ".css";
+	link.href = "./css/" + select.value + ".css";
 	link.type = "text/css";
 	link.rel = "stylesheet";
 	document.getElementsByTagName("head")[0].appendChild(link);
+	equalizeBtnSize();
 }
 
 function changeStyle(theme) {
 	document.getElementById("theme-style").href = "./css/" + theme + ".css";
-	document.cookie = document.getElementById("theme-sel").selectedIndex;
+	document.cookie = document.getElementById("theme-select").selectedIndex;
 }
 
-function toggleNew(button) {
-	let newname;
-	let dispvalue;
-	let formheight;
-	if (button.innerHTML.toLowerCase() == 'new') {
-		newname = 'hide';
-		dispvalue = 'inline';
-		formheight = '100%';
-	} else {
-		newname = 'new';
-		dispvalue = 'none';
-		formheight = getStyle(document.body, "--min-form-height");
-	}
-	button.innerHTML = newname;
-	// buttons
-	let children = button.parentNode.childNodes;
-	for (let i = 0; i < children.length; i++)
-		if (children[i] != button)
-			children[i].style.display = dispvalue;
-	// textarea
-	if (dispvalue == 'inline') {
-		button.parentNode.nextSibling.style.display = 'block';
-	}
-	else
-		button.parentNode.nextSibling.style.display = 'none';
-	// form
-	button.parentNode.parentNode.style.height = formheight;
+function newTaskNewBtnOnClick(btn) {
+	document.getElementById("newtask-form").style.height = "100%";
+	document.getElementById("newtask-submit-btn").style.display = "inline-block";
+	document.getElementById("newtask-color-select").style.display = "inline-block";
+	document.getElementById("newtask-hide-btn").style.display = "inline-block";
+	let textarea = document.getElementById("newtask-textarea");
+	textarea.style.display = "inline-block";
+	textarea.focus();
+
+	// calc form height
+	let btnContHeight = parseInt(getStyle(btn.parentNode, "height"));
+	let textareaHeigth = textarea.clientHeight;
+	let formHeight = btnContHeight + textareaHeigth + "px";
+	document.getElementById("newtask-form").style.height = formHeight;
+
+	btn.style.display = "none";
 }
 
-function setSelectColor(sel) {
-	let color = getStyle(sel.options[sel.selectedIndex], 'color');
-	sel.style.color = color;
-	sel.style.backgroundColor = color;
-	sel.parentNode.nextSibling.style.color = color;
+function newTaskHideBtnOnClick(btn) {
+	document.getElementById("newtask-form").style.height = getStyle(btn, "height");
+	document.getElementById("newtask-submit-btn").style.display = "none";
+	document.getElementById("newtask-color-select").style.display = "none";
+	document.getElementById("newtask-new-btn").style.display = "inline-block";
+	document.getElementById("newtask-textarea").style.display = "none";
+	btn.style.display = "none";
 }
 
-function toggleDone() {
-	let newname;
-	let textdec;
-	if (this.innerHTML.toLowerCase() == 'done') {
-		newname = 'undone';
-		textdec = 'line-through';
-	} else {
-		newname = 'done';
-		textdec = 'none';
-	}
-	this.innerHTML = newname;
-	this.parentNode.parentNode.nextSibling.style.textDecoration = textdec;
+function newTaskColorSelectOnChange(select) {
+	let colorName = select.options[select.selectedIndex].value;
+	let colorVar = "var(--theme-color-" + colorName + ")";
+	select.style.color = colorVar;
+	select.style.backgroundColor = colorVar;
 }
 
-function toggleEdit(obj) {
-	let newname;
-	let dispvalue;
-	let formheight;
-	if (obj.innerHTML.toLowerCase() == 'edit') {
-		newname = 'cancel';
-		dispvalue = 'inline';
-		formheight = '100%';
-	} else {
-		newname = 'edit';
-		dispvalue = 'none';
-		formheight = getStyle(document.body, "--min-form-height");
-	}
-	obj.innerHTML = newname;
-	// buttons
-	let children = obj.parentNode.childNodes;
-	for (let i = 0; i < children.length; i++)
-		if (children[i] != obj && children[i].name != 'done')
-			children[i].style.display = dispvalue;
-	// textdiv and textarea
-	let td = obj.parentNode.parentNode.nextSibling;
-	let ta = obj.parentNode.nextSibling;
-	if (dispvalue == 'inline') {
-		td.style.display = 'none';
-		ta.style.display = 'block';
-		ta.innerHTML = td.innerHTML;
-	}
-	else {
-		td.style.display = 'block';
-		ta.style.display = 'none';
-	}
-	// form
-	obj.parentNode.parentNode.style.height = formheight;
+// --- Dynamic elements ---
+
+function extractTaskIdPrefix(id) {
+	return id.match(/^task(\d*)/g);
 }
 
-function clickEdit() {
-	if (this.innerHTML.toLowerCase() == 'cancel') {
-		let td = this.parentNode.parentNode.nextSibling;
-		let color = getStyle(td, 'color');
-		this.parentNode.previousSibling.style.color = color;
-		let select = this.previousSibling.previousSibling;
-		for (let i = 0; i < select.options.length ; i++)
-			if (getStyle(select.options[i], 'color') == color) {
-				select.selectedIndex = i;
-				setSelectColor(select);
-				break;
-			}
+function removeElementById(id) {
+	var element = document.getElementById(id);
+	return element.parentNode.removeChild(element);
+}
+
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function taskRemoveBtnOnClick(btn) {
+	removeElementById(extractTaskIdPrefix(btn.id) + "-cont");
+}
+
+function taskColorSelectOnChange(select) {
+	let colorName = select.options[select.selectedIndex].value;
+	let colorVar = "var(--theme-color-" + colorName + ")";
+	select.style.color = colorVar;
+	select.style.backgroundColor = colorVar;
+	let textCont = document.getElementById(extractTaskIdPrefix(select.id) + "-text-cont");
+	textCont.style.color = colorVar;
+}
+
+function taskUndoneBtnOnClick(btn) {
+	let idPrefix = extractTaskIdPrefix(btn.id);
+	let textCont = document.getElementById(idPrefix + "-text-cont");
+	textCont.style.textDecoration = "none";
+	document.getElementById(idPrefix + "-done-btn").style.display = "inline-block";
+	btn.style.display = "none";
+}
+
+function taskDoneBtnOnClick(btn) {
+	let idPrefix = extractTaskIdPrefix(btn.id);
+	let textCont = document.getElementById(idPrefix + "-text-cont");
+	textCont.style.textDecoration = "line-through";
+	document.getElementById(idPrefix + "-undone-btn").style.display = "inline-block";
+	btn.style.display = "none";
+}
+
+function taskTextareaOnFocusOut(textarea) {
+	let idPrefix = extractTaskIdPrefix(textarea.id);
+	let textCont = document.getElementById(idPrefix + "-text-cont");
+	textCont.innerHTML = textarea.value;
+	textarea.style.display = "none";
+	
+	// set form height
+	let form = document.getElementById(idPrefix + "-form");
+	let btnCont = document.getElementById(idPrefix + "-btn-cont");
+	form.style.height = getStyle(btnCont, "height");
+
+	textCont.style.display = "block";
+}
+
+function taskTextContOnDoubleClick(textCont) {
+	textCont.style.display = "none";
+	let idPrefix = extractTaskIdPrefix(textCont.id);
+	let form = document.getElementById(idPrefix + "-form");
+	form.style.height = "100%";
+	let textarea = document.getElementById(idPrefix + "-textarea");
+	textarea.style.display = "inline-block";
+
+	// calc form height
+	let btnCont = document.getElementById(idPrefix + "-btn-cont");
+	let btnContHeight = parseInt(getStyle(btnCont, "height"));
+	let textareaHeigth = textarea.clientHeight;
+	let formHeight = btnContHeight + textareaHeigth + "px";
+	form.style.height = formHeight;
+
+	textarea.focus();
+	let undoneBtn = document.getElementById(idPrefix + "-undone-btn");
+	taskUndoneBtnOnClick(undoneBtn);
+}
+
+function createTaskDateCont(idPrefix, date) {
+	let span = document.createElement("span");
+	span.id = idPrefix + "-date";
+	span.classList.add("task-date-cont");
+	span.innerHTML = date;
+	return span;
+}
+
+function createTaskRemoveBtn(idPrefix) {
+	let btn = document.createElement("button");
+	btn.id = idPrefix + "-remove-btn";
+	btn.classList.add("task-btn");
+	btn.type = "button";
+	btn.addEventListener("click", function() {taskRemoveBtnOnClick(this);});
+	btn.style.display = 'inline-block';
+	btn.innerHTML = "remove";
+	return btn;
+}
+
+function createTaskColorOption(idPrefix, colorName) {
+	let option = document.createElement("option");
+	option.id = idPrefix + "-color-option-" + colorName;
+	option.classList.add("task-color-opt");
+	option.value = colorName;
+	option.innerHTML = "blue?";
+	return option;
+}
+
+function createTaskColorSelect(idPrefix, color) {
+	let select = document.createElement("select");
+	select.id = idPrefix + "-color-select";
+	select.classList.add("task-color-select");
+	select.addEventListener("change", function() {taskColorSelectOnChange(this);});
+	for (let i = 0; i < taskColors.length; i++) {
+		let colorOption = createTaskColorOption(idPrefix, taskColors[i]);
+		select.appendChild(colorOption);
 	}
-	toggleEdit(this);
+	select.selectedIndex = color;
+	let colorVar = "var(--theme-color-" + taskColors[color] + ")";
+	select.style.backgroundColor = select.style.color = colorVar;
+	select.style.display = "inline-block";
+	return select;
 }
 
-function changeColor() {
-	setSelectColor(this);
-	let color = getStyle(this.options[this.selectedIndex], 'color');
-	this.parentNode.previousSibling.style.color = color;
+function createTaskUndoneBtn(idPrefix, done) {
+	let btn = document.createElement("button");
+	btn.id = idPrefix + "-undone-btn";
+	btn.classList.add("task-btn");
+	btn.type = "button";
+	btn.addEventListener("click", function() {taskUndoneBtnOnClick(this);});
+	if (done == 1)
+		btn.style.display = 'inline-block';
+	btn.innerHTML = "undone";
+	return btn;
 }
 
-function applyChanges() {
-	let td = this.parentNode.parentNode.nextSibling;
-	let ta = this.parentNode.nextSibling;
-	td.innerHTML = ta.value;
-	toggleEdit(this.nextSibling.nextSibling.nextSibling);
-	let select = this.nextSibling;
-	let color = getStyle(select.options[select.selectedIndex], 'color');
-	td.style.color = color;
+function createTaskDoneBtn(idPrefix, done) {
+	let btn = document.createElement("button");
+	btn.id = idPrefix + "-done-btn";
+	btn.classList.add("task-btn");
+	btn.type = "button";
+	btn.addEventListener("click", function() {taskDoneBtnOnClick(this);});
+	if (done == 0)
+		btn.style.display = 'inline-block';
+	btn.innerHTML = "done";
+	return btn;
+}
+
+function createTaskBtnCont(idPrefix, color, done) {
+	let div = document.createElement("div");
+	div.id = idPrefix + "-btn-cont";
+	div.classList.add("task-btn-cont");
+	let removeBtn = createTaskRemoveBtn(idPrefix);
+	let colorSelect = createTaskColorSelect(idPrefix, color);
+	let undoneBtn = createTaskUndoneBtn(idPrefix, done);
+	let doneBtn = createTaskDoneBtn(idPrefix, done);
+	div.appendChild(removeBtn);
+	div.appendChild(colorSelect);
+	div.appendChild(undoneBtn);
+	div.appendChild(doneBtn);
+	return div;
+}
+
+function createTaskTextarea(idPrefix, text) {
+	let textarea = document.createElement("textarea");
+	textarea.id = idPrefix + "-textarea";
+	textarea.classList.add("task-textarea");
+	textarea.addEventListener("focusout", function() {taskTextareaOnFocusOut(this);});
+	textarea.innerHTML = text;
+	return textarea;
+}
+
+function createTaskForm(idPrefix, date, text, color, done) {
+	let form = document.createElement("form");
+	form.id = idPrefix + "-form";
+	form.classList.add("task-form");
+	let dateCont = createTaskDateCont(idPrefix, date);
+	let btnCont = createTaskBtnCont(idPrefix, color, done);
+	let taskTextarea = createTaskTextarea(idPrefix, text);
+	form.appendChild(dateCont);
+	form.appendChild(btnCont);
+	form.appendChild(taskTextarea);
+	return form;
+}
+
+function createTaskTextCont(idPrefix, text, color, done) {
+	let div = document.createElement("div");
+	div.id = idPrefix + "-text-cont";
+	div.classList.add("task-text-cont");
+	let colorVar = "var(--theme-color-" + taskColors[color] + ")";
+	div.style.color = colorVar;
+	if (done == 1)
+		div.style.textDecoration = "line-through";
+	div.innerHTML = text;
+	div.addEventListener("dblclick", function() {taskTextContOnDoubleClick(this);});
+	return div;
+}
+
+function createTaskCont(idPrefix, date, text, color, done) {
+	let div =  document.createElement("div");
+	div.id = idPrefix + "-cont";
+	div.classList.add("task-cont");
+	let brBefore = document.createElement("br");
+	let hr = document.createElement("hr");
+	let brAfter = document.createElement("br");
+	let taskForm = createTaskForm(idPrefix, date, text, color, done);
+	let taskTextCont = createTaskTextCont(idPrefix, text, color, done);
+	div.appendChild(brBefore);
+	div.appendChild(hr);
+	div.appendChild(brAfter);
+	div.appendChild(taskForm);
+	div.appendChild(taskTextCont);
+	return div;
 }
 
 function showTask(date, text, color, done) {
-	// hr
-	let br1 = document.createElement("br");
-	let br2 = document.createElement("br");
-	let hr = document.createElement("hr");
-	document.body.appendChild(br1);
-	document.body.appendChild(hr);
-	document.body.appendChild(br2);
-
-	let palette = document.getElementById("palette");
-	let colorval = getStyle(palette.options[color], "color");
-
-	// form
-	let form = document.createElement("form");
-	form.name = "task";
-	form.action = "./php/action.php";
-	form.method = "post";
-		// date
-		let datecont = document.createElement("span");
-		datecont.classList.add("date-container");
-		datecont.style.color = colorval;
-		datecont.innerHTML = date;
-		// buttons-container
-		let butcont = document.createElement("div");
-		butcont.classList.add("buttons-container");
-			// submit
-			let submit = document.createElement("button");
-//			submit.type = "submit";
-			submit.type = "button";
-			submit.classList.add("button");
-			submit.onclick = applyChanges;
-			submit.innerHTML = "ok";
-			// select
-			let select = document.createElement("select");
-			select.classList.add("palette");
-				// options
-				const colors = ["default", "red", "green", "yellow", "blue", "purple", "aqua"]
-				for (let i = 0; i < colors.length; i++) {
-					let opt = document.createElement("option");
-					opt.classList.add("color");
-					opt.value = colors[i];
-					opt.innerHTML = "blue?";
-					select.appendChild(opt);
-				}
-			select.onchange = changeColor;
-			select.selectedIndex = color;
-			select.style.backgroundColor = select.style.color = colorval;
-			// done
-			let donebut = document.createElement("button");
-			donebut.type = "button";
-			donebut.name = "done";
-			if (done == 0)
-				donebut.innerHTML = "done";
-			else
-				donebut.innerHTML = "undone";
-			donebut.classList.add("button");
-			donebut.classList.add("permbutton");
-			donebut.onclick = toggleDone;
-			// edit
-			let edit = document.createElement("button");
-			edit.type = "button";
-			edit.innerHTML = "edit";
-			edit.classList.add("button");
-			edit.classList.add("permbutton");
-			edit.onclick = clickEdit;
-		butcont.appendChild(submit);
-		butcont.appendChild(select);
-		butcont.appendChild(donebut);
-		butcont.appendChild(edit);
-		// textarea
-		let ta = document.createElement("textarea");
-		ta.name = "task";
-		ta.style.color = colorval;
-		ta.required = "required";
-	form.appendChild(datecont);
-	form.appendChild(butcont);
-	form.appendChild(ta);
-	document.body.appendChild(form);
-	// text
-	let textcont = document.createElement("div");
-	textcont.classList.add("task-text");
-	textcont.style.color = colorval;
-	if (done == 1)
-		textcont.style.textDecoration = "line-through";
-	textcont.innerHTML = text;
-	document.body.appendChild(textcont);
+	let idPrefix = "task" + (_taskIndex++);
+	let taskCont = createTaskCont(idPrefix, date, text, color, done);
+	let newTaskCont = document.getElementById("newtask-cont");
+	insertAfter(taskCont, newTaskCont);
 }
